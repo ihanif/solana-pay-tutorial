e2fad7513470eaa249ec5c34b7546e6cf7971dae
Hanif Rahman <hanifkhan@gmail.com>
2022-04-06 16:14:12 +0100

    feat: Send coupon on each purchase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import { useWallet } from '@solana/wallet-adapter-react'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import Products from '../components/Products'
-import SiteHeading from '../components/SiteHeading'
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import CouponBook from '../components/CouponBook';
+import Products from '../components/Products';
+import SiteHeading from '../components/SiteHeading';
 
 export default function HomePage() {
   // We get the public key of the connected wallet, if there is one
-  const { publicKey } = useWallet()
+  const { publicKey } = useWallet();
 
   return (
     <div className="m-auto flex max-w-4xl flex-col items-stretch gap-8 pt-24">
@@ -16,8 +17,12 @@ export default function HomePage() {
         <WalletMultiButton className="!bg-gray-900 hover:scale-105" />
       </div>
 
+      {/* We display the coupon book if there's a connected wallet */}
+      {publicKey && <CouponBook />}
+
       {/* We disable checking out without a connected wallet */}
+      {/* Also the submitTarget is /buy/transaction instead of /checkout */}
       <Products submitTarget="/checkout" enabled={publicKey !== null} />
     </div>
-  )
+  );
 }
