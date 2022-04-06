d0b293ee3da0a213965746425abb57ef6711bda6
Hanif Rahman <hanifkhan@gmail.com>
2022-04-06 16:13:10 +0100

    feat: Add coupon address

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -5,3 +5,7 @@ export const shopAddress = new PublicKey('btDWxqV1CMFTtRPq2UJpWbZYw6rfbCyRrB2Eyi
 
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+
+export const couponAddress = new PublicKey('Dw5XxEYrSrT99551QoMf7pBEfTuHznrMtozkF3v38Tyw')
+
+export const shopCouponAddress = new PublicKey('63kaq4LXEqHQjck2ATbWsERXa9zMM2VEkcBGkLwrtRBq')
