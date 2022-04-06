2c74eb0a242a456bbbd3240d70556de82afe7a85
Hanif Rahman <hanifkhan@gmail.com>
2022-04-06 13:29:04 +0100

    feat: Charge in USDC token

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -2,3 +2,6 @@ import { PublicKey } from "@solana/web3.js";
 
 // Your shop wallet address
 export const shopAddress = new PublicKey('btDWxqV1CMFTtRPq2UJpWbZYw6rfbCyRrB2Eyik7gg1');
+
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
